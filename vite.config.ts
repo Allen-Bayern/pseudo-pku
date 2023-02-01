71144fad7473f7bf5358e763b9fde00eed476409
yueb <zhaozhengyang_byn@pku.edu.cn>
2023-02-01 23:12:43 +0800

    add mock

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, ConfigEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command } : ConfigEnv) => ({
     resolve: {
         alias: {
             '@': resolve(__dirname, 'src'),
@@ -11,8 +12,13 @@ export default defineConfig({
     },
     plugins: [
         react(),
+        viteMockServe({
+            mockPath: 'mock',
+            supportTs: true,
+            localEnabled: command === 'serve',
+        }),
     ],
     server: {
         port: 4899,
     }
-});
+}));
