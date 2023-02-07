520d78b64190825c0a72dfe0c3b96704bab17dcc
yueb <zhaozhengyang_byn@pku.edu.cn>
2023-02-07 22:37:34 +0800

    底部布局完成

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,9 +1,38 @@
 import { Outlet } from "react-router-dom";
+import { useState } from 'react';
+import { Search, Tabbar, TabbarItem } from 'react-vant';
+import { InfoO, TodoListO, BullhornO, WapHomeO } from '@react-vant/icons';
+import { pkuRed } from '@/global';
 
 export default () => {
+    const [searchValue, setSearchValue] = useState<string>('');
+
     return (
-        <>
+        <div className="layout">
+            <div className="header">
+                <Search 
+                    shape="round"
+                    background={pkuRed}
+                    value={searchValue}
+                    onChange={setSearchValue}
+                    placeholder="请输入"
+                />
+            </div>
             <Outlet />
-        </>
+            <Tabbar>
+                <Tabbar.Item icon={<InfoO />}>
+                    信息服务
+                </Tabbar.Item>
+                <Tabbar.Item icon={<TodoListO />}>
+                    办事大厅
+                </Tabbar.Item>
+                <Tabbar.Item icon={<BullhornO />}>
+                    校内公告
+                </Tabbar.Item>
+                <Tabbar.Item icon={<WapHomeO />}>
+                    我的门户
+                </Tabbar.Item>
+            </Tabbar>
+        </div>
     );
 };
